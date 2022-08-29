bcbf0fb913c874be862e10fb65d4695a8414b0d1
Ziqing19 <mzq5176@gmail.com>
2022-08-29 12:50:58 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// include dependencies
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// proxy middleware options
+const options = {
+  target: 'https://arkrec.com',
+  secure: false
+};
+
+// create the proxy (without context)
+const exampleProxy = createProxyMiddleware(options);
+
+// mount `exampleProxy` in web server
+const app = express();
+app.use('*', exampleProxy);
+
+app.listen(5000);
